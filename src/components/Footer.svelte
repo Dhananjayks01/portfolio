6a6d787d95e33e5167c04c790a208db86472402f
nidhiyashwanth <nidhiyashwanth007@gmail.com>
2023-11-10 19:28:10 +0530

    first commit 😊

---
+++ src/components/Footer.svelte
@@ -0,0 +1,49 @@
+<footer class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center">
+    <p class="px-4 py-2 bg-white text-slate-950 font-medium">
+        Connect with me &darr;
+    </p>
+    <div class="flex flex-col gap-4 items-center justify-center">
+        <p>
+            <b class="pr-2">
+                GitHub
+            </b>
+            <a class="text-violet-400" href="https://github.com/nidhiyashwanth" target="_blank">
+                nidhiyashwanth<sup>
+                    <span class="text-xs scale-75 pl-0.5">
+                        <i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]">
+
+                        </i>
+                    </span>
+                </sup>
+            </a>
+        </p>
+        <p>
+            <b class="pr-2">
+                LinkedIn
+            </b>
+            <a class="text-violet-400" href="https://www.linkedin.com/in/nidhiyashwanth/" target="_blank">
+                nidhiyashwanth<sup>
+                    <span class="text-xs scale-75 pl-0.5">
+                        <i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]">
+
+                        </i>
+                    </span>
+                </sup>
+            </a>
+        </p>
+        <p>
+            <b class="pr-2">
+                Telegram
+            </b>
+            <a class="text-violet-400" href="https://t.me/nidhiyashwanth" target="_blank">
+                nidhiyashwanth<sup>
+                    <span class="text-xs scale-75 pl-0.5">
+                        <i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]">
+
+                        </i>
+                    </span>
+                </sup>
+            </a>
+        </p>
+    </div>
+</footer>
